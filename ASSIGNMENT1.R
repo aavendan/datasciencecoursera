a9c5c720bd088127b046cf64a8f674c652a88910
Allan <aavendan@gmail.com>
2018-09-18 14:09:13 -0500

    ASSIGNMENT1.R

--- ASSIGNMENT1.R
+++ ASSIGNMENT1.R
@@ -23,12 +23,35 @@ pollutantmean <- function(directory, pollutant, id=1:332) {
     
   }
   
-  total / cont
+  print(total / cont)
 }
 
-res <- pollutantmean("specdata", "sulfate", 1:10)
-print(res)
-res <- pollutantmean("specdata", "nitrate", 70:72)
-print(res)
-res <- pollutantmean("specdata", "nitrate", 23)
-print(res)
+pollutantmean("specdata", "sulfate", 1:10)
+pollutantmean("specdata", "nitrate", 70:72)
+pollutantmean("specdata", "nitrate", 23)
+
+complete <- function(directory, id = 1:332) {
+  nobs <- c()
+  
+  for(i in id) {
+    
+    number <- toString(i)
+    zeros <- paste(rep("0",(3-nchar(number))), collapse = "")
+    name <- paste(zeros, number,".csv",sep = "")
+    
+    file_name <- paste(directory,name,sep = "/")
+    
+    data <- read.csv(file_name, header = TRUE)
+    nobs <- c(nobs, nrow(data[complete.cases(data), ]))
+  }
+  
+  df <- data.frame(id, nobs)   
+  print(df)
+  
+  
+}
+
+complete("specdata", 1)
+complete("specdata", c(2, 4, 8, 10, 12))
+complete("specdata", 30:25)
+complete("specdata", 3)
